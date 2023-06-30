5c390b076d39734d5af61f48612d9721256dec7d
Khoirul Anam <khoirulanam4580@gmail.com>
2023-06-30 09:57:38 +0700

    create resources model Article & Category

--- database/migrations/2023_06_30_025323_create_articles_table.php
+++ database/migrations/2023_06_30_025323_create_articles_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('role', ['administator', 'kader']);
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('image_path');
+            $table->string('content');
+            $table->foreignId('category_id')->constrained('categories');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 };
